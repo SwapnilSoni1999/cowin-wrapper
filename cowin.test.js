b1bdb7c0c27a833b7493cfd85b607871636b9aa6
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-26 15:42:38 +0530

    write testfile for downloading certificate

--- cowin.test.js
+++ cowin.test.js
@@ -1,5 +1,6 @@
 const cowin = require('./index')
 const readline = require('readline')
+const fs = require('fs')
 
 const interface = readline.createInterface({
     input: process.stdin,
@@ -24,6 +25,21 @@ function scanf (question) {
     const pincode = await scanf('Enter your pincode (eg. 388061):')
     const centers = await cowin.getCentersByPincode(pincode)
     console.log(centers)
+
+    const mappedBenef = benef.map((b, c) => ((b.dose1_date || b.dose2_date) ? `${++c}. ${b.name}` : null), 0)
+    if (mappedBenef.length) {
+        console.log(
+            'Choose beneficiary to download certificate from:\n\t',
+            mappedBenef.join('\n\t')
+        )
+    
+        const index = await scanf('Choose Beneficiary to download Certificate:')
+        const buf = await cowin.downloadCertificatae(token, benef[+index -1].beneficiary_reference_id)
+        fs.writeFileSync('certificate.pdf', buf, { encoding: 'binary' })
+        console.log('Saved certificate!')
+    } else {
+        console.log('None of the beneficiaries got vaccinated. Hence, No certificate is there to download.')
+    }
 
     process.exit()
 })()
