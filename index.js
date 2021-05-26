25ac74b8abd20e6f540f6f9f912db076747d7342
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-26 13:11:09 +0530

    update jsDoc for functions

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
 const { default: axios } = require('axios')
-const adharValidator = require('aadhaar-validator')
 const crypto = require('crypto')
 
 const sha256 = (data) => {
     return crypto.createHash('sha256').update(data).digest('hex')
 }
 
-const headers = {
-    'authority': 'cdn-api.co-vin.in',
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
-    'accept': 'application/json, text/plain, */*',
-    'sec-ch-ua-mobile': '?0',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
-    'content-type': 'application/json',
-    'origin': 'https://selfregistration.cowin.gov.in',
-    'sec-fetch-site': 'cross-site',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-dest': 'empty',
-    'referer': 'https://selfregistration.cowin.gov.in/',
-    'accept-language': 'en-US,en-IN;q=0.9,en;q=0.8'
-}
+const httpCowin = axios.create({
+    baseURL: 'http://cdn-api.co-vin.in',
+    headers: {
+        'authority': 'cdn-api.co-vin.in',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+        'accept': 'application/json, text/plain, */*',
+        'sec-ch-ua-mobile': '?0',
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
+        'content-type': 'application/json',
+        'origin': 'https://selfregistration.cowin.gov.in',
+        'sec-fetch-site': 'cross-site',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': 'https://selfregistration.cowin.gov.in/',
+        'accept-language': 'en-US,en-IN;q=0.9,en;q=0.8'
+    }
+})
 
 class Cowin {
     /**
      * @param {number} number Mobile number 10digit
-     * @returns {string} Txn ID
+     * @returns {Promise<string>} Txn ID
      */
     static async sendOtp(number) {
-        const res = await axios({
+        const res = await httpCowin({
             method: 'POST',
             url: 'http://cdn-api.co-vin.in/api/v2/auth/generateMobileOTP',
             data: {
                 mobile: number,
                 secret: "U2FsdGVkX19at5EJPMYRe6TTDK4WWA2Nyb6b6c+QAmcYQjuhurrk6+CUqmMKHtSeaETDAIuXC+7Jz+ioZvkG+Q=="
-            },
-            headers
+            }
         })
 
         return res.data.txnId
@@ -43,31 +44,29 @@ class Cowin {
     /**
      * @param {string} txnId Transaction id from sendOtp
      * @param {number} otp otp number
-     * @returns {Array} JWT Token
+     * @returns {Promise<string>} JWT Token
      */
     static async verifyOtp(txnId, otp) {
-        const res = await axios({
+        const res = await httpCowin({
             method: 'POST',
             url: 'http://cdn-api.co-vin.in/api/v2/auth/validateMobileOtp',
             data: {
                 otp: sha256(otp),
                 txnId: txnId
-            },
-            headers
+            }
         })
         return res.data.token
     }
     
     /**
      * @param {string} token JWT token
-     * @returns {Array}
+     * @returns {Promise<Array>} beneficiaries
      */
     static async getBeneficiaries(token) {
-        const res = await axios({
+        const res = await httpCowin({
             method: 'GET',
             url: 'http://cdn-api.co-vin.in/api/v2/appointment/beneficiaries',
             headers: {
-                ...headers,
                 authorization: 'Bearer ' + token
             }
         })
@@ -75,25 +74,23 @@ class Cowin {
     }
 
     /**
-     * @returns {Array} ID types
+     * @returns {Promise<Array>} ID types
      */
     static async getIdTypes() {
-        const res = await axios({
+        const res = await httpCowin({
             method: 'GET',
             url: ' https://cdn-api.co-vin.in/api/v2/registration/beneficiary/idTypes',
-            headers
         })
         return res.data.types
     }
 
     /**
-     * @returns {Array} Gender types
+     * @returns {Promise<Array>} Gender types
      */
     static async getGenders() {
-        const res = await axios({
+        const res = await httpCowin({
             method: 'GET',
-            url: 'https://cdn-api.co-vin.in/api/v2/registration/beneficiary/genders',
-            headers
+            url: '/api/v2/registration/beneficiary/genders',
         })
         return res.data.genders
     }
@@ -101,14 +98,13 @@ class Cowin {
     /**
      * @param {Object} {Data}
      * @param {string} token JWT
-     * @returns 
+     * @returns {Promise<string>} beneficiary_reference_id
      */
     static async addBeneficiary({ name, birth_year, gender_id, photo_id_type, photo_id_number }, token) {
-        const res = await axios({
+        const res = await httpCowin({
             method: 'POST',
             url: 'http://cdn-api.co-vin.in/api/v2/registration/beneficiary/new',
             headers: {
-                ...headers,
                 authorization: 'Bearer ' + token
             },
             data: {
@@ -127,14 +123,13 @@ class Cowin {
     /**
      * @param {string} token JWT Token
      * @param {string} beneficiary_reference_id Beneficiary ID
-     * @returns {number} status code 204
+     * @returns {Promise<number>} status code 204
      */
     static async deleteBeneficiary (token, beneficiary_reference_id) {
-        const res = await axios({
+        const res = await httpCowin({
             method: 'POST',
             url: ' https://www.cowin.gov.in/api/v2/registration/beneficiary/delete',
             headers: {
-                ...headers,
                 authorization: 'Bearer ' + token
             },
             data: {
